cdffddfdea1368dba0a8f69602fc38a31f703549
Roy <royswinkels@gmail.com>
2020-10-13 22:46:50 +0200

    lesson: functions and this

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-function carPartId(name, fn) {
-  const theId = `CAR_PART_x3v0rk4`;
-  return fn(`${theId}_${name.toUpperCase()}`);
+const firstCar = { id: '5tb4gb4nj3k' };
+const secondCar = { id: 'h4ln3fbfk4' };
+const thirdCar = { id: 'rg4rn4l990' };
+
+function carPartId(name, quantity) {
+  console.log(`${this.id}_${name.toUpperCase()}_${quantity}`);
 }
 
-const carPart = carPartId('Left Door', function (id) {
-  return `Car Part ID: ${id}`;
-});
+const boundThirdCar = carPartId.bind(thirdCar);
+boundThirdCar('Windscreen', 99);
+boundThirdCar('Exhaust', 9);
 
-console.log(carPart);
+carPartId.call(firstCar, 'Left Door', 12);
+carPartId.apply(secondCar, ['Right Door', 21]);
