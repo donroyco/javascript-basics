f4d581257d8c92eea19b343a8cea6488e8f90f68
Roy <royswinkels@gmail.com>
2020-10-14 21:10:27 +0200

    lesson: object literal, function and constructor syntax

--- src/index.js
+++ src/index.js
@@ -5,17 +5,10 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
 
-const firstCar = { id: '5tb4gb4nj3k' };
-const secondCar = { id: 'h4ln3fbfk4' };
-const thirdCar = { id: 'rg4rn4l990' };
+const drink = {
+  id: 'xhs8Pla',
+  name: 'Coca-Cola',
+  price: 99
+};
 
-function carPartId(name, quantity) {
-  console.log(`${this.id}_${name.toUpperCase()}_${quantity}`);
-}
-
-const boundThirdCar = carPartId.bind(thirdCar);
-boundThirdCar('Windscreen', 99);
-boundThirdCar('Exhaust', 9);
-
-carPartId.call(firstCar, 'Left Door', 12);
-carPartId.apply(secondCar, ['Right Door', 21]);
+console.log(drink);
