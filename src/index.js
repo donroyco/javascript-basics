09a3ad7c38a75b071a369f8c71c7c9b545b2e2f0
Roy <royswinkels@gmail.com>
2020-10-14 21:21:52 +0200

    lesson: properties, methods and values

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 const drink = {
   id: 'xhs8Pla',
   name: 'Coca-Cola',
-  price: 99
+  price: 99,
+  getDrinkDetails() {
+    return `Drink ${this.name} (${this.price})`;
+  },
+  // Arrow function
+  // getDrinkDetails: () => `Drink ${drink.name} (${drink.price})`,
+  //
+  // Every key is converted to a string
+  'abc 123': 'I am the value!',
+  100: 'I am a number'
 };
 
-console.log(drink);
+console.log(drink.getDrinkDetails());
