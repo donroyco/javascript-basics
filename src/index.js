28b1bcd2129598bed71295f39ae47a9ede1594ed
Roy <royswinkels@gmail.com>
2020-10-14 21:43:27 +0200

    lesson: destructuring object properties

--- src/index.js
+++ src/index.js
@@ -8,16 +8,28 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 const drink = {
   id: 'xhs8Pla',
   name: 'Coca-Cola',
-  price: 99,
-  getDrinkDetails() {
-    return `Drink ${this.name} (${this.price})`;
+  price: {
+    sale: 99,
+    full: 129,
   },
-  // Arrow function
-  // getDrinkDetails: () => `Drink ${drink.name} (${drink.price})`,
-  //
-  // Every key is converted to a string
-  'abc 123': 'I am the value!',
-  100: 'I am a number'
 };
 
-console.log(drink.getDrinkDetails());
+// Nested object fetch value of 'full' key
+const { id, name, price: { full } } = drink;
+
+console.log( id, name, full );
+
+// Move this block to line 17
+// When duplicate identifier key, you can rename them in destructured object
+// const id = 1234;
+
+// Renamed to myId
+// const { id: myId, name, price: { full } } = drink;
+//
+// console.log( id, myId, name, full );
+// ---------------
+
+
+// Use spread operator to get the rest of object keys
+// const { id, name, ...rest } = drink;
+// console.log( id, name, rest );
