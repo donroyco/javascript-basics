1a81217772e4da6201d20865cc348744454cb996
Roy <royswinkels@gmail.com>
2020-10-18 20:25:21 +0200

    lesson: declarative object iteration

--- src/index.js
+++ src/index.js
@@ -14,14 +14,8 @@ const drink = {
   }
 };
 
-// Loop over all object keys
-// for...in
-for (const prop in drink) {
-  const value = drink[prop];
-  // Check if iterable in nested loop
-  if (Object.prototype.toString.call(value) === '[object Object]') {
-    for (const key in value) {
-      console.log(key);
-    }
-  }
-}
+// Declarative approach for object iteration
+Object.keys(drink).forEach(prop => console.log(drink[prop]));
+
+console.log('-------');
+console.log(drink);
