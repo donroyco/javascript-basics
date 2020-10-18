b2a6c677a3db7e992e01c6140dc248abb36ebd5e
Roy <royswinkels@gmail.com>
2020-10-18 20:35:56 +0200

    lesson: properties, indexes and elements

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-
+// Terminology: Item in array = element
 const drinks = ['Lemonade', 'Lime', 'Peach'];
 
 console.log(drinks);
+console.log('----------');
 
-// Alternative way to declare arrays (not recommended)
-console.log(new Array('Lemonade', 'Lime', 'Peach'));
-console.log(Array('Lemonade', 'Lime', 'Peach'));
+// Access last item in array
+const itemIndex = drinks.length - 1;
+console.log(drinks[itemIndex]);
+
+// How to empty array
+drinks.length = 0;
+// Remove elements from array
+drinks.splice(0, drinks.length);
+console.log(drinks);
