53e79c63e4d3780334394cc62d036851e4922ea4
Roy <royswinkels@gmail.com>
2020-10-26 09:25:13 +0100

    lesson: finding array elements

--- src/index.js
+++ src/index.js
@@ -6,18 +6,27 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // ----
 const drinks = ['Lemonade', 'Lime', 'Peach'];
 
-// Beginning of array
-drinks.shift(); // mutable: changes original
-const newDrinksArray = [ ...drinks.slice()]; // immutable: new array
-console.log(newDrinksArray);
+// Find a particular element (returns an index)
+const index = drinks.indexOf('Lime');
+if (index !== -1) {
+  console.log(drinks[index]);
+}
 
-// Middle of array (anywhere)
-const index = drinks.length - 1;
-const newDrinks = [...drinks.slice(0, index), ...drinks.slice(index + 1)];
-console.log(newDrinks);
+// Included (returns a boolean)
+const included = drinks.includes('Peach');
+console.log(included);
 
-// End of array
-const removed = drinks.pop();
-console.log(removed);
+// Find in different data structure
+const drinksWithId = [
+  { id: 1, name: 'Lemonade' },
+  { id: 2, name: 'Lime' },
+  { id: 3, name: 'Peach' },
+];
 
-console.log(drinks);
+// findIndex is the same as 'indexOf', but easier for complex data structures
+const idIndex = drinksWithId.findIndex(value => value.name === 'Peach');
+console.log(drinksWithId[idIndex]);
+
+// find is the same as 'includes', but easier for complex data structures
+const foundItem = drinksWithId.find(value => value.name === 'Peach');
+console.log(foundItem);
