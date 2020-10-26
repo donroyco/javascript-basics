f4eff0a759f7fccc919b7a2e5ddbbdb644804948
Roy <royswinkels@gmail.com>
2020-10-26 09:30:18 +0100

    lesson: shallow and deep array cloning

--- src/index.js
+++ src/index.js
@@ -4,29 +4,18 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-const drinks = ['Lemonade', 'Lime', 'Peach'];
+const drinks = [['Lemonade', 99], ['Lime', 99], ['Peach', 89]];
 
-// Find a particular element (returns an index)
-const index = drinks.indexOf('Lime');
-if (index !== -1) {
-  console.log(drinks[index]);
-}
+// Shallow clone = one level down
+// const drinksClone = [...drinks];
+// const drinksClone = drinks.slice();
+// const drinksClone = Array.from(drinks);
 
-// Included (returns a boolean)
-const included = drinks.includes('Peach');
-console.log(included);
+// Deep clone
+const drinksClone = JSON.parse(JSON.stringify(drinks));
 
-// Find in different data structure
-const drinksWithId = [
-  { id: 1, name: 'Lemonade' },
-  { id: 2, name: 'Lime' },
-  { id: 3, name: 'Peach' },
-];
+drinksClone[0][1] = 1000;
 
-// findIndex is the same as 'indexOf', but easier for complex data structures
-const idIndex = drinksWithId.findIndex(value => value.name === 'Peach');
-console.log(drinksWithId[idIndex]);
+console.log(drinks);
+console.log(drinksClone);
 
-// find is the same as 'includes', but easier for complex data structures
-const foundItem = drinksWithId.find(value => value.name === 'Peach');
-console.log(foundItem);
