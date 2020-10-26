5d699e21463915f39535ced7c321411eb6ef83fa
Roy <royswinkels@gmail.com>
2020-10-26 17:55:42 +0100

    lesson: imperative array interation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-console.log(typeof []);
-console.log([] instanceof Array);
-console.log(new Array() instanceof Array);
-console.log(Array.isArray([1,2,3,4])); // Best way to check if it's an array, new way to do so
-console.log(Object.prototype.toString.call([]));
+const drinks = ['Pepsi', 'Lemonade', 'Cola'];
+
+for (let i = 0; i < drinks.length; i++) {
+  console.log(`#${i}`)
+  const drink = drinks[i];
+  if (drink === 'Lemonade') {
+    console.log('Pour me a drink');
+  }
+}
 
